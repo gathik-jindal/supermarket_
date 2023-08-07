cf4c07d8f917f55cb8b45b4f308fe58ffa00ba53
boom29 <Gathik.Jindal@iiitb.ac.in>
2023-08-07 23:02:21 +0530

    played around created a new class but not seeing any use

--- item_handling.py
+++ item_handling.py
@@ -1,5 +1,15 @@
 import csv
+from item_class import item
 
-headers = ["Serial number", "Item name", "Price", "Price type", "Discount"]
+def add_items(writer, l):
+    item_ = item(writer, l[0], l[1], l[2], l[3], l[4])
+    
+    item_.write_row()
 
-fh = open("item_Database.csv", "w")
+fh = open("item_Database.csv", "a+")
+writer = csv.DictWriter(fh, fieldnames=["Serial number", "Item name", "Price", "Price type", "Discount"])
+writer.writeheader()
+
+list_of_items = []
+
+fh.close()
