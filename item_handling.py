8a9b86471aed2d2f20377b4e188d2517e5c76ae0
boom29 <Gathik.Jindal@iiitb.ac.in>
2023-08-12 01:47:38 +0530

    Added search command

--- item_handling.py
+++ item_handling.py
@@ -1,34 +1,83 @@
 import csv
 from item_class import item
+import panda as pd
 
 HEADER = ["Serial number", "Item name", "Price", "Price type", "Discount"]
 
 # add item function
 
 def add_item():
-    serial_num = input("Enter serial number: ")
-    item_name = input("Enter item name: ")
-    price = input("Item price: ")
-    price_type = input("Price type (if by quantity type \"qty\", if by bunch \"bnch\"): ")
-    discount = input("Enter discount on price: ")
 
-    if price == '':
-        price = 0
-    if price_type == '':
-        price_type = "bnch"
-    if discount == '':
-        discount = 0
+    with open("item_Database.csv", "r+", newline='') as fh:
+        writer = csv.DictWriter(fh, fieldnames=HEADER)
 
-    item1 = item(serial_num, item_name, price, price_type, discount)
+        serial_num = input("Enter serial number: ")
+        item_name = input("Enter item name: ")
+        price = input("Item price: ")
+        price_type = input("Price type (if by quantity type \"qty\", if by bunch \"bnch\"): ")
+        discount = input("Enter discount on price: ")
 
-    fh.seek(0, 2)
-    item1.write_row(writer)
+        if price == '':
+            price = 0
+        if price_type == '':
+            price_type = "bnch"
+        if discount == '':
+            discount = 0
 
-def search_item():
-    pass
+        item1 = item(serial_num, item_name, price, price_type, discount)
+
+        fh.seek(0, 2)
+        item1.write_row(writer)
+
+        print("written row")
+
+def search_item(serial_or_name="serial"):
+    
+    with open("item_Database.csv", "r", newline='') as fh:
+        reader = csv.DictReader(fh, fieldnames=HEADER)
+
+        pos = fh.tell()
+
+        match serial_or_name:
+
+            case "serial":
+                find_serial = input("Enter serial number: ")
+
+                for row in reader:
+                    if row[HEADER[0]] == find_serial:
+                        print(row)
+                        break
+
+            case "name":
+                find_name = input("Enter name: ")
+
+                for row in reader:
+                    if row[HEADER[1]] == find_name:
+                        print(row)
+                        break
+
+        print("completed search")
+
+    return pos
 
 def delete_item():
-    pass
+    
+    pos = search_item()
+
+    with open("item_Database.csv", "r+") as fh:
+        writer = csv.DictWriter(fh, fieldnames=HEADER)
+
+        fh.seek(pos)
+
+        writer.writerow({
+            "Serial number": None,
+            "Item name": None,
+            "Price": None,
+            "Price type": None,
+            "Discount": None
+            })
+        
+        print("deletion complete")
 
 
 # Initialising file
@@ -39,15 +88,11 @@ try:
 except FileNotFoundError:
     fh = open("item_Database.csv", "w", newline='')
     file_does_not_exist = True
-fh.close()
 
-fh = open("item_Database.csv", "r+", newline='')
 writer = csv.DictWriter(fh, fieldnames=HEADER)
-reader = csv.DictReader(fh, fieldnames=HEADER)
-
 if file_does_not_exist:
     writer.writeheader()
+fh.close()
 
 if __name__ == "__main__":
-    add_item()
-    fh.close()
+    delete_item()
